bb13577239cef3e51de81d72579a2aeb282f3868
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-02 10:24:48 -0300

    btnMenu

--- page7/script.js
+++ page7/script.js
@@ -1,10 +1,15 @@
 const menuMobile = document.querySelector('.menuMobile')
 const btnMenu = document.querySelector('.btnMenuMobile')
+const fundo = document.querySelector('main')
 
 btnMenu.addEventListener('click', ()=>{
-    if(menuMobile.style.left == '-100%' ){
-        menuMobile.style.left = '0%' 
-    }else{
+    btnMenu.classList.toggle('openMenu')
+    if(menuMobile.style.left == '0%' ){
         menuMobile.style.left = '-100%' 
+        // fundo.style.opacity = '1'
+    }else{
+        menuMobile.style.left = '0%' 
+        // fundo.style.opacity = '0.5'
+        
     }
 })
