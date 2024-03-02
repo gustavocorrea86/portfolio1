0411062c8df6dd9bfa6c6aee7624764b0c0d2fe9
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-02 15:22:34 -0300

    ajuste

--- page7/script.js
+++ page7/script.js
@@ -4,14 +4,13 @@ const fundo = document.querySelector('main')
 
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('openMenu')
-    menuMobile.classList.toggle('hide')
-    menuMobile.style.transition = '6s'
-    // if(menuMobile.style.left == '0%' ){
-    //     menuMobile.style.left = '-100%' 
-    //     // fundo.style.opacity = '1'
-    // }else{
-    //     menuMobile.style.left = '0%' 
-    //     // fundo.style.opacity = '0.5'
+    // menuMobile.classList.toggle('hide')
+    if(menuMobile.style.left == '0%' ){
+        menuMobile.style.left = '-100%' 
+        // fundo.style.opacity = '1'
+    }else{
+        menuMobile.style.left = '0%' 
+        // fundo.style.opacity = '0.5'
         
-    // }
+    }
 })
