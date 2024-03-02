09369d9e0e37f3cabf9ca3a5fb9d3d1f46eebd02
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-02 18:21:04 -0300

    actions header

--- page7/script.js
+++ page7/script.js
@@ -1,6 +1,8 @@
 const menuMobile = document.querySelector('.menuMobile')
 const btnMenu = document.querySelector('.btnMenuMobile')
 const fundo = document.querySelector('main')
+const carts = document.querySelector('.carts')
+const descResum = document.querySelector('.descResum')
 
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('openMenu')
@@ -11,6 +13,8 @@ btnMenu.addEventListener('click', ()=>{
     }else{
         menuMobile.style.left = '0%' 
         // fundo.style.opacity = '0.5'
-        
     }
+})
+carts.addEventListener('click', ()=>{
+    descResum.classList.toggle('hide')
 })
