3004a9f03e25218c80be457f37c0b8055ae6511d
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-02 14:08:29 -0300

    ajustando toggle

--- page7/script.js
+++ page7/script.js
@@ -4,12 +4,14 @@ const fundo = document.querySelector('main')
 
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('openMenu')
-    if(menuMobile.style.left == '0%' ){
-        menuMobile.style.left = '-100%' 
-        // fundo.style.opacity = '1'
-    }else{
-        menuMobile.style.left = '0%' 
-        // fundo.style.opacity = '0.5'
+    menuMobile.classList.toggle('hide')
+    menuMobile.style.transition = '6s'
+    // if(menuMobile.style.left == '0%' ){
+    //     menuMobile.style.left = '-100%' 
+    //     // fundo.style.opacity = '1'
+    // }else{
+    //     menuMobile.style.left = '0%' 
+    //     // fundo.style.opacity = '0.5'
         
-    }
+    // }
 })
