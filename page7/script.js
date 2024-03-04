d1093ec1be4c6c2950c12ac66e20d353814e4038
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-04 17:09:26 -0300

    atualizando js

--- page7/script.js
+++ page7/script.js
@@ -5,6 +5,14 @@ const carts = document.querySelector('.carts')
 const resumCart = document.querySelector('.resumCart')
 const btnAddAndCart = document.querySelector('.btnAddAndCart')
 const back = document.querySelector('.back')
+const sub = document.querySelector('.sub')
+const qtd = document.querySelector('.qtd')
+let qtdAdd = 0
+const add = document.querySelector('.add')
+const shop = document.querySelector('.shop')
+const shopQtd = document.querySelector('.shopQtd')
+let cartQtd = 0
+
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('openMenu')
     if(menuMobile.style.left == '0%' ){
@@ -21,4 +29,24 @@ carts.addEventListener('click', ()=>{
         resumCart.style.display ='block'
     }
     // resumCart.classList.toggle('hide')
+})
+
+add.addEventListener('click', ()=>{
+    qtdAdd += 1
+    cartQtd +=1
+    shop.style.display = 'flex'
+    qtd.innerHTML = qtdAdd
+    shopQtd.innerHTML = cartQtd
+})
+
+sub.addEventListener('click',()=>{
+    if(qtdAdd > 0 && cartQtd > 0){
+        qtdAdd -= 1
+        cartQtd -=1
+    }
+    if(qtdAdd == 0){
+        shop.style.display = 'none'
+    }
+    qtd.innerHTML = qtdAdd
+    shopQtd.innerHTML = cartQtd
 })
