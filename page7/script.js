d6383848ac51dd165e4b67c5095f9f2c57b007ef
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-03-04 09:13:01 -0300

    atualizando

--- page7/script.js
+++ page7/script.js
@@ -3,13 +3,15 @@ const btnMenu = document.querySelector('.btnMenuMobile')
 const fundo = document.querySelector('main')
 const carts = document.querySelector('.carts')
 const resumCart = document.querySelector('.resumCart')
-
+const btnAddAndCart = document.querySelector('.btnAddAndCart')
+const back = document.querySelector('.back')
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('openMenu')
     if(menuMobile.style.left == '0%' ){
         menuMobile.style.left = '-100%' 
     }else{
         menuMobile.style.left = '0%' 
+        menuMobile.style.zIndex = '1'
     }
 })
 carts.addEventListener('click', ()=>{
