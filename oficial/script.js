b75cfa697f155e07c96e347b5f9de77e1820b531
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-05-28 17:43:28 -0300

    mobile

--- oficial/script.js
+++ oficial/script.js
@@ -1,39 +1,9 @@
 const btnMenu = document.querySelector('.iconMenu')
 const boxMenu = document.querySelector('.boxMenu')
 
-// btnMenu.addEventListener('click', ()=>{
-//     btnMenu.classList.toggle('openMenu')
-//     menuMobile.style.transition = "1s ease";
-//     if( menuMobile.style.left == '0%'){
-//         menuMobile.style.left = '100%'
-//     }else{
-//         menuMobile.style.left = '0%'
-//     }
-// })
-
 const servicos = document.querySelector('.servicos')
 const img5 = document.querySelector('.img5')
 
-btnMenu.addEventListener('click',()=>{
-    if(boxMenu.style.visibility === 'hidden'){
-        boxMenu.style.visibility = 'hidden'
-        
-    }else{
-       
-        ScrollReveal().reveal('.boxMenu',{
-            duration: 1500,
-            distance:'500px',
-            origin: 'rigth'
-        })
-    }
-    
-})
-
-
-
-// servicos.addEventListener('click', ()=>{
-//    img5.scrollIntoView({behavior:'smooth'})
-// })
 
 window.sr = ScrollReveal({reset: true})
 
