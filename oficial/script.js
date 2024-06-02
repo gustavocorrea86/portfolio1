0ae65105c58bb4fbd9d0fa46e550afa0f5a6a487
Gustavo Correa <154375048+gustavocorrea86@users.noreply.github.com>
2024-06-02 10:35:08 -0300

    mobile

--- oficial/script.js
+++ oficial/script.js
@@ -49,20 +49,123 @@ ScrollReveal().reveal('.h3quemsou',{
 ScrollReveal().reveal('.h3apresentacao',{
     duration: 1500,
     distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.secHabilidades',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.html',{
+    duration: 1500,
+    distance:'500px',
     origin: 'left'
 })
 
-// const texto = document.querySelector('.h3apresentacao')
-// function typeWriter(elemento){
-//     const textoArray = elemento.innerHTML.split('')
-//     elemento.innerHTML = ''
-//     textoArray.forEach(function(letra, i){
-//         setTimeout(function(){
-//             elemento.innerHTML += letra
-//         }, 75 * i)
-//     })
-//    }
-//    typeWriter(texto)
+ScrollReveal().reveal('.js',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.css',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'top'
+})
+
+ScrollReveal().reveal('.node',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.mysql',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'top'
+})
+
+ScrollReveal().reveal('.mongo',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.bootstrap',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.figma',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'top'
+})
+
+ScrollReveal().reveal('.ilustrator',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.secProjRealizados',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.proj1',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.proj1Mobile',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.proj2',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.proj3',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.proj4',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.proj5',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
+ScrollReveal().reveal('.proj6',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'left'
+})
+
+ScrollReveal().reveal('.proj7',{
+    duration: 1500,
+    distance:'500px',
+    origin: 'rigth'
+})
+
 
 
 
